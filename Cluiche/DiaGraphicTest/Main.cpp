e56946364f7fb07a20d9e6db013ede262a67f823
Conor Lenihan <lenihan.conor@gmail.com>
2015-06-29 21:16:42 -0500

    Making DiaGraphics a threaded solution

--- Cluiche/DiaGraphicTest/Main.cpp
+++ Cluiche/DiaGraphicTest/Main.cpp
@@ -2,7 +2,8 @@
 #include <DiaSFML/RenderWindow.h>
 #include <DiaCore/Time/TimeServer.h>
 #include <DiaCore/Time/TimeAbsolute.h>
-#include <DiaCore/Time/TimeThreadLimiter.h>
+#include <DiaCore/Timer/TimeThreadLimiter.h>
+#include <DiaCore/Frame/FrameStream.h>
 #include <DiaInput/IInputSource.h>
 #include <DiaGraphics/Interface/ICanvas.h>
 
@@ -20,7 +21,15 @@ struct SimThreadStruct
 {
 public:
 
-	SimThreadStruct()
+	SimThreadStruct(bool& running,
+					const Dia::Core::TimeServer& timeServer,
+					Dia::Core::FrameStream<Dia::Input::EventData>& inputToSimFrameStream,
+					Dia::Core::FrameStream<Dia::Graphics::FrameData>& SimToRenderFrameStream)
+		: mRunning(running)
+		, mTimeServer(timeServer)
+		, mInputToSimFrameStream(inputToSimFrameStream)
+		, mSimToRenderFrameStream(SimToRenderFrameStream)
+		, mThreadLimiter(30.0) 
 	{}
 
 	void operator()()
@@ -31,24 +40,96 @@ public:
 	void Run()
 	{
 		// This is where the simulation (Model) would go...
-		// In good architetcure you would only do simualtion events here and have a seperate step to interpret that into game specific rendering
 
 		// This below is just a test
-		mRenderFrameBuffer.RequestDraw(Dia::Graphics::DebugFrameDataCircle2D(Dia::Maths::Vector2D(100.0f, 100.0f), 75.0f));
-		mRenderFrameBuffer.RequestDraw(Dia::Graphics::DebugFrameDataCircle2D(dynamicCirclePos, 25.0f, dynamicCircleColour));
-		mRenderFrameBuffer.RequestDraw(Dia::Graphics::DebugFrameDataLine2D(Dia::Maths::Vector2D(100.0f, 100.0f), dynamicCirclePos));
+		while (mRunning)
+		{
+			mThreadLimiter.Start();
+
+			// All going to Sim thread
+
+			Dia::Core::Containers::DynamicArrayC<const Dia::Input::EventData*, 32> inputEventPtrBuffer;
+			mInputToSimFrameStream.FetchAllDataUpToTime(mTimeServer.GetTime(), inputEventPtrBuffer);
+
+			for (unsigned int i = 0; i < inputEventPtrBuffer.Size(); i++)
+			{
+				const Dia::Input::EventData* pEventData = inputEventPtrBuffer[i];
+
+				// This is where an application will handle any user input
+				for (unsigned int j = 0; j < pEventData->Size(); j++)
+				{
+					Dia::Input::Event event = (*pEventData)[j];
+					switch (event.type)
+					{
+					case Dia::Input::Event::EType::kClosed:
+						// end the program
+						mRunning = false;
+						break;
+					case Dia::Input::Event::EType::kConsoleGamepadAnalogStickMove:
+						dynamicCirclePos += Dia::Maths::Vector2D(event.consoleGamepadMoveEvent.x, -event.consoleGamepadMoveEvent.y);
+						break;
+					case Dia::Input::Event::EType::kConsoleGamepadButtonPressed:
+						switch (event.consoleGamepadButtonEvent.button)
+						{
+						case Dia::Input::ConsoleGamepad::EButtonID::A:
+							dynamicCircleColour = Dia::Graphics::RGBA::Green;
+							break;
+						case Dia::Input::ConsoleGamepad::EButtonID::B:
+							dynamicCircleColour = Dia::Graphics::RGBA::Red;
+							break;
+						case Dia::Input::ConsoleGamepad::EButtonID::X:
+							dynamicCircleColour = Dia::Graphics::RGBA::Blue;
+							break;
+						case Dia::Input::ConsoleGamepad::EButtonID::Y:
+							dynamicCircleColour = Dia::Graphics::RGBA::Yellow;
+							break;
+						}
+
+						break;
+					default:
+						break;
+					}
+				}
+			}
+
+			mInputToSimFrameStream.GarbageCollectAllFramesOlderThan(mTimeServer.GetTime());
+
+			Dia::Graphics::FrameData renderFrameBuffer;
+
+			renderFrameBuffer.RequestDraw(Dia::Graphics::DebugFrameDataCircle2D(Dia::Maths::Vector2D(100.0f, 100.0f), 75.0f));
+			renderFrameBuffer.RequestDraw(Dia::Graphics::DebugFrameDataCircle2D(dynamicCirclePos, 25.0f, dynamicCircleColour));
+			renderFrameBuffer.RequestDraw(Dia::Graphics::DebugFrameDataLine2D(Dia::Maths::Vector2D(100.0f, 100.0f), dynamicCirclePos));
+
+			mSimToRenderFrameStream.InsertCopyOfDataToStream(renderFrameBuffer, mTimeServer.GetTime());
+
+			mThreadLimiter.Stop();
+			std::cout << "SIM: Wait " << mThreadLimiter.RemainingTime().AsIntInMilliseconds() << " ms\n";
+			mThreadLimiter.SleepThread();
+		}
 	}
 
-	Dia::Graphics::FrameData mRenderFrameBuffer;
-
+private:
+	// Shared resources
+	bool& mRunning;
+	const Dia::Core::TimeServer& mTimeServer;
+	Dia::Core::FrameStream<Dia::Input::EventData>& mInputToSimFrameStream;
+	Dia::Core::FrameStream<Dia::Graphics::FrameData>& mSimToRenderFrameStream;
+	
+	// Local resources
+	Dia::Core::TimeThreadLimiter mThreadLimiter;
 };
 
 struct RenderThreadStruct
 {
 public:
-	RenderThreadStruct(const bool& running, Dia::Graphics::ICanvas* pCanvas)
+	RenderThreadStruct(const bool& running, 
+						const Dia::Core::TimeServer& timeServer, 
+						Dia::Core::FrameStream<Dia::Graphics::FrameData>& frameStream, 
+						Dia::Graphics::ICanvas* pCanvas)
 		: mRunning(running)
-		, mThreadLimiter(30.0)
+		, mTimeServer(timeServer)
+		, mFrameStream(frameStream)
+		, mThreadLimiter(60.0)
 		, mpCanvas(pCanvas)
 	{}
 
@@ -57,46 +138,42 @@ public:
 		Run();
 	}
 
-	void PushRenderBuffer(const Dia::Graphics::FrameData& renderFrameBuffer)
-	{
-		{
-			std::lock_guard<std::mutex> lock(mRenderFrameBufferMutex);
-
-			mRenderFrameBuffer.Clear();
-			mRenderFrameBuffer.Copy(renderFrameBuffer);
-		}
-	}
-
 	void Run()
 	{
 		// Render (View) Part
-		// In good architecture this is completley generic.
 		while (mRunning)
 		{
 			mThreadLimiter.Start();
 			
+			Dia::Core::TimeAbsolute timeStampOfRenderFrameBuffer = Dia::Core::TimeAbsolute::Zero();
+			const Dia::Graphics::FrameData* pRenderFrameBuffer = mFrameStream.FetchDataClosestToTime(mTimeServer.GetTime(), timeStampOfRenderFrameBuffer);
+			
+			if (pRenderFrameBuffer == nullptr)
 			{
-				std::lock_guard<std::mutex> lock(mRenderFrameBufferMutex);
-
-				mpCanvas->StartFrame(mRenderFrameBuffer);
-				mpCanvas->ProcessFrame(mRenderFrameBuffer);
-				mpCanvas->EndFrame(mRenderFrameBuffer);
+				continue;
 			}
+					
+			mpCanvas->StartFrame(*pRenderFrameBuffer);
+			mpCanvas->ProcessFrame(*pRenderFrameBuffer);
+			mpCanvas->EndFrame(*pRenderFrameBuffer);
+			
+			mFrameStream.GarbageCollectAllFramesOlderThan(timeStampOfRenderFrameBuffer);
 
 			mThreadLimiter.Stop();
-			
-			std::cout << "Wait " << mThreadLimiter.RemainingTimeInMilliseconds() << " ms\n";
+			std::cout << "RENDER: Wait " << mThreadLimiter.RemainingTime().AsIntInMilliseconds() << " ms\n";
 			mThreadLimiter.SleepThread();
 		}
 	}
 
 private:
+	// Shared resources
 	const bool& mRunning; 
-	Dia::Core::TimeThreadLimiter mThreadLimiter;
+	const Dia::Core::TimeServer& mTimeServer;
+	Dia::Core::FrameStream<Dia::Graphics::FrameData>& mFrameStream;
 	Dia::Graphics::ICanvas* mpCanvas;
 
-	std::mutex mRenderFrameBufferMutex;
-	Dia::Graphics::FrameData mRenderFrameBuffer;
+	// Local resources
+	Dia::Core::TimeThreadLimiter mThreadLimiter;
 };
 
 int main(int argc, const char* argv[])
@@ -115,81 +192,57 @@ int main(int argc, const char* argv[])
 	// Set up generic input sources
 	Dia::Input::InputSourceManager inputSourceManager;
 	Dia::Input::ConsoleGamepadManager gamepadManager;
+
 	// We are using SFML for keyboard and mouse support
 	renderWindow->ListenForInputSources(Dia::Core::BitArray8(Dia::SFML::InputSource::ESources::kSystem | Dia::SFML::InputSource::ESources::kKeyboard | Dia::SFML::InputSource::ESources::kMouse));	// We are getting mouse and keyboard only from SFML
 	inputSourceManager.AddInputSource(renderWindow);
 	// We are using a dia specific source for the gamepad
 	inputSourceManager.AddInputSource(&gamepadManager); // Getting gamepads from the DIA
 	
-	Dia::Core::TimeServer timeServer(30.0f, Dia::Core::TimeAbsolute::Zero());	// With only one time server everything in the main loop will increment at its frequency
+	Dia::Core::TimeServer timeServer(60.0f, Dia::Core::TimeAbsolute::Zero());	// With only one time server everything in the main loop will increment at its frequency
 
 	canvas->SetActiveContext(false); // Need to disable active context before starting rendering thread
 
 	bool running = true;
+	Dia::Core::FrameStream<Dia::Input::EventData> inputToSimFrameStream;
+	Dia::Core::FrameStream<Dia::Graphics::FrameData> simToRenderFrameStream;
 
-	RenderThreadStruct renderThreadStruct(running, canvas);
+	SimThreadStruct simThreadStruct(running, timeServer, inputToSimFrameStream, simToRenderFrameStream);
+	std::thread simThread(std::ref(simThreadStruct));
+
+	RenderThreadStruct renderThreadStruct(running, timeServer, simToRenderFrameStream, canvas);
 	std::thread renderThread(std::ref(renderThreadStruct));
 
 	// run the main loop
-	
+	// Main thread
+	Dia::Core::TimeThreadLimiter threadLimiter(1.0f/timeServer.GetStep().AsFloatInSeconds());
 	while (running)
 	{
-		{
-			inputSourceManager.StartFrame();
-			
-	//		Register
+		threadLimiter.Start();
 
-			Dia::Input::EventStream eventStream; // Scope will wipe this for next turn
+		// Input thread :)
+		{
+			Dia::Input::EventData eventData;
 
 			// Input (Control) part		
-			inputSourceManager.Update(eventStream);
-
-			// Hmmm... this probably belongs in simulation as it is the interpetation of the buttons
-			// This is where an application will handle any user input
-			for (unsigned int i = 0; i < eventStream.Size(); i++)
-			{
-				Dia::Input::Event event = eventStream[i];
-				switch (event.type)
-				{
-				case Dia::Input::Event::EType::kClosed:
-					// end the program
-					running = false;
-					break;
-				case Dia::Input::Event::EType::kConsoleGamepadAnalogStickMove:
-					dynamicCirclePos += Dia::Maths::Vector2D(event.consoleGamepadMoveEvent.x, -event.consoleGamepadMoveEvent.y);
-					break;
-				case Dia::Input::Event::EType::kConsoleGamepadButtonPressed:
-					switch (event.consoleGamepadButtonEvent.button)
-					{
-					case Dia::Input::ConsoleGamepad::EButtonID::A:
-						dynamicCircleColour = Dia::Graphics::RGBA::Green;
-						break;
-					case Dia::Input::ConsoleGamepad::EButtonID::B:
-						dynamicCircleColour = Dia::Graphics::RGBA::Red;
-						break;
-					case Dia::Input::ConsoleGamepad::EButtonID::X:
-						dynamicCircleColour = Dia::Graphics::RGBA::Blue;
-						break;
-					case Dia::Input::ConsoleGamepad::EButtonID::Y:
-						dynamicCircleColour = Dia::Graphics::RGBA::Yellow;
-						break;
-					}
-					
-					break;
-				default:
-					break;
-				}
-			}
-
+			inputSourceManager.StartFrame();
+			inputSourceManager.Update(eventData);
 			inputSourceManager.EndFrame();
+			
+			if (eventData.Size() > 0)
+			{
+				inputToSimFrameStream.InsertCopyOfDataToStream(eventData, timeServer.GetTime());
+			}
 		}
 
-		SimThreadStruct simThreadStruct;
-		simThreadStruct.Run();
-	
-		renderThreadStruct.PushRenderBuffer(simThreadStruct.mRenderFrameBuffer);
+		timeServer.Tick();
+
+		threadLimiter.Stop();
+		std::cout << "Main: Wait " << threadLimiter.RemainingTime().AsIntInMilliseconds() << " ms\n";
+		threadLimiter.SleepThread();
 	}
 
+	simThread.join();
 	renderThread.join();
 
 	windowFactory.Destroy(window);
