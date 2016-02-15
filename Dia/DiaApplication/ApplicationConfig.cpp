8b67851825d68419f11820f0dff8c73c2e4d6d7c
LenihanConor <lenihan.conor@gmail.com>
2016-02-15 17:15:21 -0600

    Adding this in to stop some compilation error

---
+++ Dia/DiaApplication/ApplicationConfig.cpp
@@ -0,0 +1,12 @@
+#include "DiaApplication/ApplicationConfig.h"
+
+#include "DiaCore/Type/TypeDefinitionMacros.h"
+
+namespace Dia
+{
+	namespace Application
+	{
+		DIA_TYPE_DEFINITION(Config)
+		DIA_TYPE_DEFINITION_END()
+	}
+}
---
+++ Dia/DiaApplication/ApplicationConfig.h
@@ -0,0 +1,17 @@
+#pragma
+
+#include "DiaCore/Type/TypeDeclarationMacros.h"
+
+namespace Dia
+{
+	namespace Application
+	{	
+		class Config
+		{
+		public:
+			DIA_TYPE_DECLARATION;
+
+		private:
+		};
+	}
+}
